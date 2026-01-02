a840ab3bd88774234d018d5adb358466ff8f98fa
Manjunath M Budagatti <manjunath@rpncsystems.com>
2026-01-02 14:43:58 +0530

    New Updated Vegetable 4 Code

---
+++ MVC_UI_Templates/Controllers/Vegetable4Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_UI_Templates.Controllers // Make sure this namespace matches your project name
+{
+    public class Vegetable4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
